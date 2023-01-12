d03eee418d8b7ff899114761025db7a372264688
alkatsalex <alkatsalex@ukr.net>
2023-01-12 12:27:30 +0200

    add moduls

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css"
+
+import { galleryItems } from './gallery-items.js'
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(`.gallery`)
+
+const marckup = galleryItems.map(({preview, original, alt ,description,}) => 
+    `<div class="gallery">
+    <a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="" title="Beautiful Image"/></a>
+</div>`
+).join("")
+
+gallery.insertAdjacentHTML("beforeend", marckup)
+
+gallery.addEventListener(`click`, choosePhoto)
+
+function choosePhoto(e) {
+    if (!e.target.classList.contains(`gallery__image`)) {
+        return
+    }
+    }
+    var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250});
+      console.log(lightbox);
